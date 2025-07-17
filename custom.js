10be205edea42b5d2c619407e90ef7c32266423b
nagyeong91 <nacong486@gmail.com>
2025-07-17 23:12:32 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,51 @@
+$(function(){
+
+  //메뉴//
+  $(".nav>ul>li").mouseover(function(){
+    $(this).find(".sub").stop().slideDown()
+  })
+  $(".nav>ul>li").mouseout(function(){
+    $(this).find(".sub").stop().slideUp()
+  })
+
+
+  //슬라이드//
+  var i = 0
+
+  function slide(){
+    if(i<2){
+      i++
+    }else{
+      i=0
+    }
+
+  $(".slider ul li").fadeOut()
+  $(".slider ul li").eq(i).fadeIn()
+  }
+  
+  setInterval(slide,2000)
+
+
+  //팝업//
+  $("button").click(function(){
+    $(".popup").hide()
+  })
+
+  
+  //스크롤//
+  $(window).on('scroll', function () {
+    $('.scroll-target').each(function () {
+      var $el = $(this);
+      var scrollTop = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      var offsetTop = $el.offset().top;
+
+      if (scrollTop + windowHeight > offsetTop + 50) {
+        if (!$el.hasClass('show')) {
+          $el.addClass('show');
+        }
+      }
+    });
+  });
+
+})
